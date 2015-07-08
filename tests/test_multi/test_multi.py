44aedacdaf91e865c29648dcbdfa87b883a6b9d8
Brad Smith <brad.alan.smith@gmail.com>
2015-07-07 20:37:18 -0700

    added the new routes to the test and improved testdocs

--- tests/test_multi/test_multi.py
+++ tests/test_multi/test_multi.py
@@ -31,21 +31,25 @@ class TestMulti(unittest.TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    def get_index_soup(self):
-    	return BeautifulSoup(self.app.get('/').data, 'html.parser')
+    def get_soup(self, url):
+    	return BeautifulSoup(self.app.get(url).data, 'html.parser')
 
     def test_scripts_loaded(self):
-    	"""Does the number of script elements match the route in `site.json`?"""
+    	"""Does the number of scripts match the spec for each route?"""
         cfg = json.load(open('site.json'))
-        print cfg
-    	soup = self.get_index_soup()
+    	soup = self.get_soup('/')
     	self.assertEqual(len(list(soup.find_all('script'))),3)
+        soup = self.get_soup('/a')
+        self.assertEqual(len(list(soup.find_all('script'))),2)
+        soup = self.get_soup('/b')
+        self.assertEqual(len(list(soup.find_all('script'))),1)
 
     def test_scripts_200(self):
-        """Are each of the scripts for this route available?"""
-        soup = self.get_index_soup()
-        for script in soup.find_all('script'):
-            self.app.get(script['src'])
+        """Are each of the scripts for each route available?"""
+        for url in ('/','/a','/b'):
+            soup = self.get_soup(url)
+            for script in soup.find_all('script'):
+                self.app.get(script['src'])
 
 
 if __name__ == '__main__':
