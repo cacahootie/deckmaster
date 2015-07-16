04b9187f2f322ede2f5e0d77ae535c1f90ed21c9
Brad Smith <brad.alan.smith@gmail.com>
2015-07-16 09:50:23 -0700

    actual git stuff

--- deckmaster/app/gitloader.py
+++ deckmaster/app/gitloader.py
@@ -11,4 +11,12 @@ from flask import request
 class GitLoader(jinja2.BaseLoader):
     """Load a template from a specific git revision."""
     def get_source(self, environment, template):
-        return open(template).read(), template, lambda: True
+        if '@' in request.url:
+            base, revid = request.path.rsplit('@',1)
+            print 'git show %s %s ' % (revid, template)
+            src = check_output(
+                ['git show %s %s ' % (revid, template)],
+                shell=True
+            )
+            return src, base, lambda: True
+        return open(template).read(), template, lambda: False
--- tests/test_gitloader/test_gitloader.py
+++ tests/test_gitloader/test_gitloader.py
@@ -5,8 +5,10 @@ import sys
 import os
 
 sys.path.append('../..')
+import deckmaster
 from deckmaster.app import GitLoader
 
+app = deckmaster.app.get_instance()
 
 class TestGitLoader(unittest.TestCase):
 
@@ -14,8 +16,21 @@ class TestGitLoader(unittest.TestCase):
         os.path.dirname(os.path.abspath(__file__)), 'templates'
     )
 
-    def test_load(self):
-        print GitLoader().get_source(None, os.path.join(self.templdir, 'test.txt'))
+    def test_file_load(self):
+        """Can the loader get a simple, local file?"""
+        with app.test_request_context('/test.txt'):
+            src, templ, reload = GitLoader().get_source(
+                None, os.path.join(self.templdir, 'test.txt')
+            )
+            self.assertEqual(src, '{{ burgers }}\n{{ fries }}\n')
+
+    def test_git_load(self):
+        """Can the loader get a template at a specific revision?"""
+        with app.test_request_context('/test.txt@c89d6c7'):
+            src, templ, reload = GitLoader().get_source(
+                None, os.path.join(self.templdir, 'test.txt')
+            )
+            self.assertEqual(src, '{{ burgers }}\n{{ fries }}\n')
 
 
 if __name__ == '__main__':
